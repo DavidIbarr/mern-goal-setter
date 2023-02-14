0a723b4e2f71b33b49b867f09d3bee3c270e5cf7
david.ibarra1452@gmail.com <77758167+DavidIbarr@users.noreply.github.com>
2023-02-14 10:40:58 -0600

    Feature: Add Error Handler for Routes

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,20 +1,26 @@
+const asyncHandler = require('express-async-handler')
+
 // TODO: docs
 
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
 	res.status(200).json({ message: 'Get goals' })
-}
+})
 
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
+	if(!req.body.text) {
+		res.status(400)
+		throw new Error('Please add a text field')
+	}
 	res.status(200).json({ message: 'Set goal' })
-}
+})
 
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
 	res.status(200).json({ message: `Update goal ${req.params.id}` })
-}
+})
 
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
 	res.status(200).json({ message: `Delete goal ${req.params.id}` })
-}
+})
 
 
 module.exports = {
