9f32dc6c2c2f4bad4a48fd3a916fe03b85249c74
david.ibarra1452@gmail.com <77758167+DavidIbarr@users.noreply.github.com>
2023-02-14 09:46:37 -0600

    finished basic CRUD routes

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
+
+router.get('/', getGoals).post('/', setGoal)
+
+router.put('/:id', updateGoal).delete('/:id', deleteGoal)
+
+module.exports = router
