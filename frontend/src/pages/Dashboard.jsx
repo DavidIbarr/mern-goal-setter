4d94901a53fefa02480bf613176387df21c97e71
david.ibarra1452@gmail.com <77758167+DavidIbarr@users.noreply.github.com>
2023-02-26 22:44:42 -0600

    Feat: Finish UI for Listing Goals, Redux Goal State and Goal Service

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,26 +1,57 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import Spinner from '../components/Spinner'
 import GoalForm from '../components/GoalForm'
+import GoalItem from '../components/GoalItem'
+import { getGoals, reset } from '../features/goals/goalSlice'
 
 function Dashboard() {
 	const navigate = useNavigate()
 
+	// initialize redux state
+	const dispatch = useDispatch()
 	const { user } = useSelector((state) => state.auth)
+	const { goals, isLoading, isError, message } = useSelector((state) => state.goals)
 
 	// if the user is not logged in, redirect to login page
 	useEffect(() => {
+		if (isError) {
+			console.log(message)
+		}
 		if (!user) {
 			navigate('/login')
 		}
-	}, [user, navigate])
+		// put the goals from the backend into the goals[] array
+		dispatch(getGoals())
+		// reset the state and clear the dashboard at the end of the component lifecycle
+		return () => {
+			dispatch(reset())
+		}
+	}, [user, navigate, isError, message, dispatch])
+
+	if (isLoading) {
+		return <Spinner />
+	}
 
 	return (
 		<>
 		<section className='heading'>
 			<h1>Welcome {user && user.name}</h1>
 			<p>Goals Dashboard</p>
-			<GoalForm />
+		</section>
+		<GoalForm />
+		<section className='content'>
+			{goals.length > 0 ? (
+				<div className='goals'>
+					{goals.map((goal) => (
+						<GoalItem key={goal._id} goal={goal} />
+					))}
+				</div>
+			) : (
+				<h3>You have not set any goals</h3>
+			)
+			}
 		</section>
 		</>
 	)
